32d940582e90a4cb2147dbaaa3174f83647921eb
Themike <magsa37p1@gmail.com>
2024-12-18 11:35:54 +0700

    Refactor
    Delete console log

--- services/cloudService.js
+++ services/cloudService.js
@@ -11,31 +11,24 @@ const bucket = storage.bucket('mearn-app-01');
 const uploadImage = async (file) => {
     try {
         const { originalname, buffer, mimetype } = file;
-        
         const timestamp = Date.now(); // Ensure a timestamp is available
         const blob = bucket.file(timestamp+originalname.replace(/ /g, "_") ); // Replace spaces with underscores in the filename
-        
         const blobStream = blob.createWriteStream({
             metadata: {
                 contentType: mimetype,
             },
         });
-        
         return new Promise((resolve, reject) => {
             blobStream.on('error', (err) => {
-                console.log("ssssssssssss:",err);
                 reject(err); // Reject the promise if there is an error
             });
-
             blobStream.on('finish', () => {
                 const publicUrl = `https://storage.googleapis.com/${bucket.name}/${blob.name}`;
                 resolve(publicUrl); // Resolve the promise with the URL once upload finishes
             });
-
             blobStream.end(buffer);
         });
     } catch (error) {
-        console.error("Error uploading image:", error);
         throw error; // Propagate the error to be handled outside
     }
 };
--- services/tokenService.js
+++ services/tokenService.js
@@ -8,36 +8,24 @@ const initialzeSecret = async (req, res) => {
   try {
     const secret = crypto.randomBytes(64).toString('hex'); // สุ่ม JWT Secret
     let envContent = '';
-    
-    // อ่านไฟล์ .env ถ้ามีอยู่
     if (fs.existsSync('.env')) {
       envContent = fs.readFileSync('.env', 'utf8');
     }
-
-    // แยกบรรทัดออกมา
     let lines = envContent.split('\n');
     let foundSecret = false;
-
-    // ตรวจสอบและอัปเดตเฉพาะ SECRET
     lines = lines.map(line => {
       if (line.startsWith('SECRET=')) {
         foundSecret = true;
-        return `SECRET=${secret}`; // แทนที่ค่า SECRET เดิม
+        return `SECRET=${secret}`; 
       }
-      return line; // ไม่เปลี่ยนแปลงบรรทัดอื่น
+      return line;
     });
 
-    // ถ้าไม่มี SECRET อยู่ในไฟล์ ให้เพิ่มใหม่
     if (!foundSecret) {
       lines.push(`SECRET=${secret}`);
     }
-
-    // รวมเนื้อหาไฟล์กลับ
     envContent = lines.join('\n');
-
-    // เขียนกลับไปที่ไฟล์
     fs.writeFileSync('.env', envContent, 'utf8');
-    console.log("Secret was created");
   } catch (err) {
     console.log("Error:", err);
   }
@@ -66,13 +54,12 @@ const secretWork = async (req,res) =>{
     timezone: "Asia/Bangkok" // กำหนดให้ทำงานตามโซนเวลาไทย
 })
 }
-
 /**** middleware verfiy header acess token *****/
 const verifyToken = jwt({
   secret: process.env.SECRET,
   algorithms: ["HS256"], 
   userProperty: "auth", 
-});//send user collect in req.auth
+});
 
 const handleTokenError = (err, res, next) => {
   if (err) {
