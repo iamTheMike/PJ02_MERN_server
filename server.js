6102e9f677ec7c19302af2c449c45368dd45f116
Themike <magsa37p1@gmail.com>
2025-01-05 17:24:12 +0700

    Turn off corn service

--- server.js
+++ server.js
@@ -31,5 +31,5 @@ app.listen( port, async ()=>{
     console.log(`start server on port ${port}`);
    await initializeBlogDatabase();
    await initializeAuthDatabase();
-   await secretWork();
+//    await secretWork();
 })
